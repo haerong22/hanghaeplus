82c96017887beeedaa641e98e82a7567141a3878
haerong22 <haerong22@naver.com>
2024-03-26 19:44:12 +0900

    feat: clean architecture(db) - 엔티티 제약조건 추가

--- week2-clean-architecture/infrastructure/db/src/main/kotlin/io/hhplus/cleanarchitecture/jpa/lecture/LectureApplicantEntity.kt
+++ week2-clean-architecture/infrastructure/db/src/main/kotlin/io/hhplus/cleanarchitecture/jpa/lecture/LectureApplicantEntity.kt
@@ -4,8 +4,17 @@ import io.hhplus.cleanarchitecture.jpa.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 
-@Table(name = "lecture_applicant")
+@Table(
+    name = "lecture_applicant",
+    uniqueConstraints = [
+        UniqueConstraint(
+            name = "unique_key_with_user_id_and_lecture_id",
+            columnNames = ["lecture_id", "user_id"]
+        )
+    ]
+)
 @Entity
 internal class LectureApplicantEntity(
 
